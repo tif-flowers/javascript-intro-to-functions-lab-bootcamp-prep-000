8d0929a89926cd7728600682080a9d27454ac4c2
Tif Flowers <tif@gentlebull.com>
2018-02-05 20:21:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,4 +18,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase() )
   return "I can't hear you!"
+  else if (string === string.toUpperCase() )
+  return "YES INDEED!"
+  else (string === "I love you, Grandma.")
+  return "I love you, too."
 }
