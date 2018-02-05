e283e73ac3fdc51921837e29c18c566406eab973
Tif Flowers <tif@gentlebull.com>
2018-02-05 19:29:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ function whisper(string) {
 function logShout(string) {
   console.log ('HELLO') 
 }
+
+function logWhisper(string) {
+  console.log ('hello') 
+}
 var uppercase = "HELLO!"
  
 uppercase.toUpperCase() === uppercase // true
